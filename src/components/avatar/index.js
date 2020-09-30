47e66f1adaf85ba70d7af87629b1c7c562928713
panheng <yaowen.pan@hotmail.com>
2020-09-30 10:05:15 +0800

    add: first commit

---
+++ src/components/avatar/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export default (props) => {
+    return <div className='avatar'>123</div>
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '@/assets/style/index.less'
+import Avatar from '@/components/avatar'
+export { Avatar }
